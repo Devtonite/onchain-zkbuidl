10d3695abbbc4aa4e51f5657c071b8f774ab3add
devtonite <devtonite001@gmail.com>
2023-12-15 21:10:35 -0600

    Add all necessary contract functions v1

--- src/Core.ts
+++ src/Core.ts
@@ -1,4 +1,4 @@
-import { Field, SmartContract, state, State, method, Bool, Poseidon } from 'o1js';
+import { Field, SmartContract, state, State, method, Bool, Poseidon, Provable } from 'o1js';
 import { stringToFields } from 'o1js/dist/node/bindings/lib/encoding';
 
 export class Core extends SmartContract {
@@ -6,6 +6,12 @@ export class Core extends SmartContract {
   @state(Bool) isBountyOpen = State<Bool>();
   
   @state(Field) solutionCommit = State<Field>();
+  
+  // @state(Bool) builderResult = State<Bool>();
+  // @state(Bool) hunterResult = State<Bool>();
+  // @state(Bool) verifierResult = State<Bool>();
+
+  @state(Field) hashOfComputation = State<Field>();
   @state(Bool) isVerified = State<Bool>();
 
   init() {
@@ -13,6 +19,7 @@ export class Core extends SmartContract {
     this.testCommmit.set(Field(0));
     this.solutionCommit.set(Field(0));
     this.isBountyOpen.set(Bool(false));
+    this.hashOfComputation.set(Field(0));
     this.isVerified.set(Bool(false));
   }
 
@@ -42,4 +49,36 @@ export class Core extends SmartContract {
     this.solutionCommit.set(solutionHash);
     this.isBountyOpen.set(Bool(false));
   }
+
+  // method to compute the solution with the revealed unit test and commit that solution on chain
+  @method computeSolution(unitTest: Field, codeSolution: Field, hashOfComputation: Field, result: Bool) {
+    this.isBountyOpen.requireEquals(Bool(false));
+    this.isVerified.requireEquals(Bool(false));
+    this.testCommmit.requireEquals(unitTest);
+    this.solutionCommit.requireEquals(codeSolution);
+    this.hashOfComputation.requireEquals(Field(0));
+
+    let resultHash = Poseidon.hash([this.testCommmit.get(), this.solutionCommit.get()]);
+    resultHash.assertEquals(Poseidon.hash([unitTest, codeSolution]));
+
+    result.assertEquals(Bool(true));
+
+    this.hashOfComputation.set(hashOfComputation);
+  }
+  
+  // method to commit to a solution hash for open bounty
+  @method verifySolution(unitTest: Field, codeSolution: Field, hashOfComputation: Field, result: Bool) {
+    this.isBountyOpen.requireEquals(Bool(false));
+    this.isVerified.requireEquals(Bool(false));
+    this.testCommmit.requireEquals(unitTest);
+    this.solutionCommit.requireEquals(codeSolution);
+    this.hashOfComputation.get().assertNotEquals(Field(0));
+    
+    let resultHash = Poseidon.hash([this.testCommmit.get(), this.solutionCommit.get(), this.hashOfComputation.get()]);
+    resultHash.assertEquals(Poseidon.hash([unitTest, codeSolution, hashOfComputation]))
+    result.assertEquals(Bool(true));
+    
+    this.isVerified.set(Bool(true));
+  }
+
 }
