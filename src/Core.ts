f462743ae1c5b3820cb8b1698ae6faaa1cf5124e
devtonite <devtonite001@gmail.com>
2023-12-19 17:13:16 -0600

    Add preconditions to verify correct state prior to method call

--- src/Core.ts
+++ src/Core.ts
@@ -2,34 +2,29 @@ import { Field, SmartContract, state, State, method, Bool, Poseidon, Provable } 
 
 export class Core extends SmartContract {
   @state(Field) testCommmit = State<Field>();
-  @state(Bool) isBountyOpen = State<Bool>();
-  
   @state(Field) solutionCommit = State<Field>();
-  
-  // @state(Bool) builderResult = State<Bool>();
-  // @state(Bool) hunterResult = State<Bool>();
-  // @state(Bool) verifierResult = State<Bool>();
-
   @state(Field) hashOfComputation = State<Field>();
+  @state(Bool) isBountyOpen = State<Bool>();
   @state(Bool) isVerified = State<Bool>();
 
   init() {
     super.init();
     this.testCommmit.set(Field(0));
     this.solutionCommit.set(Field(0));
-    this.isBountyOpen.set(Bool(false));
     this.hashOfComputation.set(Field(0));
+    this.isBountyOpen.set(Bool(false));
     this.isVerified.set(Bool(false));
   }
 
-  // method to start a new bounty
+  // method to commit to a unit test
   @method publishBounty(testHash: Field) {
     this.testCommmit.requireEquals(Field(0));
     this.solutionCommit.requireEquals(Field(0));
+    this.hashOfComputation.requireEquals(Field(0));
     this.isBountyOpen.requireEquals(Bool(false));
     this.isVerified.requireEquals(Bool(false));
 
-    let emptyTest = this.testCommmit.get();
+    let emptyTest = this.testCommmit.getAndRequireEquals();
     testHash.assertNotEquals(emptyTest);
 
     this.testCommmit.set(testHash);
@@ -37,47 +32,47 @@ export class Core extends SmartContract {
 
   }
 
-  // method to commit to a solution hash for open bounty
+  // method to commit to a solution for an open bounty
   @method commitSolution(solutionHash: Field) {
+    this.solutionCommit.requireEquals(Field(0));
+    this.hashOfComputation.requireEquals(Field(0));
     this.isBountyOpen.requireEquals(Bool(true));
     this.isVerified.requireEquals(Bool(false));
-    let emptySolution = this.solutionCommit.getAndRequireEquals();
 
+    let emptySolution = this.solutionCommit.getAndRequireEquals();
     solutionHash.assertNotEquals(emptySolution);
 
     this.solutionCommit.set(solutionHash);
     this.isBountyOpen.set(Bool(false));
   }
 
-  // method to compute the solution with the revealed unit test and commit that solution on chain
+  // method to compute the solution with the revealed unit test, and commit that computation on chain
   @method computeSolution(unitTest: Field, codeSolution: Field, hashOfComputation: Field, result: Bool) {
-    this.isBountyOpen.requireEquals(Bool(false));
-    this.isVerified.requireEquals(Bool(false));
     this.testCommmit.requireEquals(unitTest);
     this.solutionCommit.requireEquals(codeSolution);
-    
     this.hashOfComputation.requireEquals(Field(0));
-
-    let resultHash = Poseidon.hash([this.testCommmit.get(), this.solutionCommit.get()]);
-    resultHash.assertEquals(Poseidon.hash([unitTest, codeSolution]));
-
+    this.isBountyOpen.requireEquals(Bool(false));
+    this.isVerified.requireEquals(Bool(false));
+    
+    let testAndSolHash = Poseidon.hash([this.testCommmit.getAndRequireEquals(), this.solutionCommit.getAndRequireEquals()]);
+    testAndSolHash.assertEquals(Poseidon.hash([unitTest, codeSolution]));
     result.assertEquals(Bool(true));
+
     this.hashOfComputation.set(hashOfComputation);
   }
   
-  // method to commit to a solution hash for open bounty
+  // method to verify the commited computation of the commited solution against the committed unit test for 
   @method verifySolution(unitTest: Field, codeSolution: Field, hashOfComputation: Field, result: Bool) {
     this.isBountyOpen.requireEquals(Bool(false));
     this.isVerified.requireEquals(Bool(false));
     this.testCommmit.requireEquals(unitTest);
     this.solutionCommit.requireEquals(codeSolution);
-
     this.hashOfComputation.requireEquals(hashOfComputation);
     
-    let resultHash = Poseidon.hash([this.testCommmit.get(), this.solutionCommit.get()]);
-    resultHash.assertEquals(Poseidon.hash([unitTest, codeSolution]));
-
+    let testAndSolHash = Poseidon.hash([this.testCommmit.getAndRequireEquals(), this.solutionCommit.getAndRequireEquals()]);
+    testAndSolHash.assertEquals(Poseidon.hash([unitTest, codeSolution]));
     result.assertEquals(Bool(true));
+
     this.isVerified.set(Bool(true));
   }
 
