38eae684c4dc4ca83e5a7bfac2c0b9d0eb782669
devtonite <devtonite001@gmail.com>
2023-12-25 07:44:40 -0600

    Add working interaction test with solidity contract example

--- src/tests/SolContract.test.ts
+++ src/tests/SolContract.test.ts
@@ -2,11 +2,22 @@ import { stringToFields } from 'o1js/dist/node/bindings/lib/encoding';
 import { Core } from '../Core';
 import { Field, Mina, PrivateKey, PublicKey, AccountUpdate, Poseidon, Bool } from 'o1js';
 import { Bounty } from '../BountyType';
+import * as fs from 'fs';
+
 
 export type user = {
     privateKey: PrivateKey,
     publicKey: PublicKey
 }
+
+export type publicState = {
+    testHash: Field,
+    solutionHash: Field,
+    rawTest: string,
+    rawSolution: string,
+}
+
+let proofsEnabled = true;
 
 describe('Solidity', () => {
     let deployer: user, 
@@ -16,10 +27,12 @@ describe('Solidity', () => {
     let zkAppAddress: PublicKey,
         zkAppPrivateKey: PrivateKey,
         zkApp: Core;
+    let defaultEmptyHash = Poseidon.hash(stringToFields(''));
+    let publiclyViewableState: publicState;
 
     beforeAll(async () => {
         await Core.compile();
-        const Local = Mina.LocalBlockchain();
+        const Local = Mina.LocalBlockchain({proofsEnabled});
         Mina.setActiveInstance(Local);
 
         deployer = Local.testAccounts[0];
@@ -30,14 +43,150 @@ describe('Solidity', () => {
         zkAppPrivateKey = PrivateKey.random();
         zkAppAddress = zkAppPrivateKey.toPublicKey();
         zkApp = new Core(zkAppAddress);
+
+        publiclyViewableState = {
+            testHash: Field(0),
+            solutionHash: Field(0),
+            rawTest: '',
+            rawSolution: '',
+        }
     })
 
-    it('deployer deploys zkApp smart contract', async () => {
+    it('DEPLOYER deploys zkApp smart contract', async () => {
         const txn = await Mina.transaction(deployer.publicKey, () => {
             AccountUpdate.fundNewAccount(deployer.publicKey);
             zkApp.deploy();
           });
           await txn.prove();
           await txn.sign([deployer.privateKey, zkAppPrivateKey]).send();
+
+          let bountyState = new Bounty({
+            testCommit: defaultEmptyHash,
+            solutionCommit: defaultEmptyHash,
+            computationCommit: defaultEmptyHash,
+            isBountyOpen: Bool(false),
+            isVerified: Bool(false),
+          })
+
+          expectedOnChainState(zkApp, bountyState);
     });
+
+    it('BUILDER publishes a unit test bounty', async () => {
+        let testFileHash = getFileHash('./src/builder/bountyTest.sol.txt')
+        
+        const txn = await Mina.transaction(builder.publicKey, () => {
+          zkApp.publishBounty(testFileHash);
+        });
+        await txn.prove();
+        await txn.sign([builder.privateKey]).send();
+    
+        let bountyState = new Bounty({
+            testCommit: Poseidon.hash([testFileHash]),
+            solutionCommit: defaultEmptyHash,
+            computationCommit: defaultEmptyHash,
+            isBountyOpen: Bool(true),
+            isVerified: Bool(false),
+          })
+        
+        expectedOnChainState(zkApp, bountyState);
+        publiclyViewableState.testHash = bountyState.testCommit;
+      });
+
+      it('HUNTER commits to a viable code solution', async () => {
+        let solutionFileHash = getFileHash('./src/hunter/bountySolution.sol.txt')
+        
+        const txn = await Mina.transaction(hunter.publicKey, () => {
+          zkApp.commitSolution(solutionFileHash);
+        });
+        await txn.prove();
+        await txn.sign([hunter.privateKey]).send();
+    
+        let bountyState = new Bounty({
+            testCommit: publiclyViewableState.testHash,
+            solutionCommit: Poseidon.hash([solutionFileHash]),
+            computationCommit: defaultEmptyHash,
+            isBountyOpen: Bool(false),
+            isVerified: Bool(false),
+          })
+        
+        expectedOnChainState(zkApp, bountyState);
+        publiclyViewableState.solutionHash = bountyState.solutionCommit
+      });
+
+      it('BUILDER publicly releases unit test after 3 days of submissions',async () => {
+        // BUILDER waits 3 days for hunter to commit to a solution
+        publiclyViewableState.rawTest = './src/builder/bountyTest.sol.txt';
+      })
+
+      it('HUNTER computes unit test with code solution and commits to result (after waiting period)', async () => {
+        let testFileHash = getFileHash(publiclyViewableState.rawTest);
+        let solutionFileHash = getFileHash('./src/hunter/bountySolution.sol.txt')
+        let [computation, result] = runSolutionAgainstTest();
+        let computationHash = Poseidon.hash(stringToFields(computation))
+
+        const txn = await Mina.transaction(hunter.publicKey, () => {
+          zkApp.computeSolution(testFileHash, solutionFileHash, computationHash, result);
+        });
+        await txn.prove();
+        await txn.sign([hunter.privateKey]).send();
+    
+        let bountyState = new Bounty({
+            testCommit: publiclyViewableState.testHash,
+            solutionCommit: publiclyViewableState.solutionHash,
+            computationCommit: Poseidon.hash([computationHash]),
+            isBountyOpen: Bool(false),
+            isVerified: Bool(false),
+          })
+        
+        expectedOnChainState(zkApp, bountyState);
+        publiclyViewableState.rawSolution = './src/hunter/bountySolution.sol.txt';
+      });
+
+      it('VERIFIER runs unit test with code solution to verify the HUNTER\'s passsing result', async () => {
+        let testFileHash = getFileHash(publiclyViewableState.rawTest);
+        let solutionFileHash = getFileHash(publiclyViewableState.rawSolution);
+        let [computation, result] = runSolutionAgainstTest();
+        let computationHash = Poseidon.hash(stringToFields(computation))
+
+        const txn = await Mina.transaction(hunter.publicKey, () => {
+          zkApp.verifySolution(testFileHash, solutionFileHash, computationHash, result);
+        });
+        await txn.prove();
+        await txn.sign([hunter.privateKey]).send();
+    
+        let bountyState = new Bounty({
+            testCommit: publiclyViewableState.testHash,
+            solutionCommit: publiclyViewableState.solutionHash,
+            computationCommit: Poseidon.hash([computationHash]),
+            isBountyOpen: Bool(false),
+            isVerified: Bool(true),
+          })
+        
+        expectedOnChainState(zkApp, bountyState);
+      });
+
+
+    function expectedOnChainState(contract: Core, bounty: Bounty) {
+        expect(contract.testCommmit.get()).toEqual(bounty.testCommit);
+        expect(contract.solutionCommit.get()).toEqual(bounty.solutionCommit);
+        expect(contract.computationCommit.get()).toEqual(bounty.computationCommit);
+        expect(contract.isBountyOpen.get()).toEqual(bounty.isBountyOpen);
+        expect(contract.isVerified.get()).toEqual(bounty.isVerified);
+    }
+
+    function getFileHash(textFilePath: string): Field {
+        let string = fs.readFileSync(textFilePath, 'utf-8');
+        let hash = Poseidon.hash(stringToFields(string));
+        return hash;
+    }
+
+    function getFile(textFilePath: string): string {
+        let fileString = fs.readFileSync(textFilePath, 'utf-8');
+        return fileString;
+    }
+
+    function runSolutionAgainstTest(): [string, Bool] {
+        // todo: run the foundry unit test with the commited solution
+        return ['The test ran successfully', Bool(true)];
+    }
 });
