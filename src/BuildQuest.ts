605b0de653c8268407302599540365650a597d7f
devtonite <devtonite001@gmail.com>
2024-01-21 23:28:29 -0600

    Add verifyTestHash zkprogram

--- src/BuildQuest.ts
+++ src/BuildQuest.ts
@@ -31,3 +31,26 @@ export class VerifyBuilderIDCircuitProof extends ZkProgram.Proof(
     VerifyBuilderIDCircuit
 ){}
 
+function verifyTestHash(quest: BountyQuest, rawTestHash: Field ): BountyQuest {
+    let questTestHash = Poseidon.hash([rawTestHash]);
+    questTestHash.assertEquals(quest.testHash);
+    return quest;
+}
+
+export const VerifyTestHashCircuit = ZkProgram({
+    name: "add test hash",
+    publicInput: BountyQuest,
+    publicOutput: BountyQuest,
+
+    methods: {
+
+        addTestHash: {
+            privateInputs: [Field],
+            method: verifyTestHash,
+        },
+    },
+});
+
+export class VerifyTestHashCircuitProof extends ZkProgram.Proof(
+    VerifyTestHashCircuit
+){}
