4b2461494383d66c5cde15b7c3ceaa9b53fbebe7
devtonite <devtonite001@gmail.com>
2024-01-22 20:49:33 -0600

    Add commitBountyKey method in Core contract

--- src/Core.ts
+++ src/Core.ts
@@ -1,29 +1,46 @@
 import { Field, SmartContract, state, State, method, Bool, Poseidon, UInt32, UInt64, PrivateKey, AccountUpdate } from 'o1js';
 import { BountyQuest } from './builder/BuildQuest';
+import { BountyKey } from './hunter/CommitKey';
 
 export class Core extends SmartContract {
 
   @state(BountyQuest) openBountyQuest = State<BountyQuest>();
-  @state(Field) merkledBountyKeys = State<Field>();
+  @state(BountyKey) merkledBountyKeys = State<BountyKey>();
 
   init() {
     super.init();
-    this.merkledBountyKeys.set(Field(0));
-
   }
 
   @method publishBountyQuest(quest: BountyQuest) {    
     let allAccountUpdates = AccountUpdate.create(this.sender)
+
     let balance = allAccountUpdates.account.balance.getAndRequireEquals();
     let questTokenReward = quest.tokenRewardAmount;
     balance.assertGreaterThanOrEqual(questTokenReward)
     allAccountUpdates.send({to: this.address, amount: questTokenReward})
     allAccountUpdates.requireSignature();
-    this.openBountyQuest.set(quest);
+
+    let duration = quest.bountyDuration;
+    let now = this.network.timestamp.getAndRequireEquals();
+    let bountyOpenTill = now.add(duration);
+
+    let timedQuest = new BountyQuest({
+      builderID: quest.builderID,
+      testHash: quest.testHash,
+      tokenRewardAmount: quest.tokenRewardAmount,
+      bountyDuration: bountyOpenTill
+    })
+
+    this.openBountyQuest.set(timedQuest);
 
   }
 
-  @method commitBountyKey(solution: Field) {
+  @method commitBountyKey(solution: BountyKey) {
+    let openBounty = this.openBountyQuest.getAndRequireEquals();
+    let now = this.network.timestamp.getAndRequireEquals();
+    now.assertLessThanOrEqual(openBounty.bountyDuration);
+    this.merkledBountyKeys.set(solution)
+
   }
   
   @method rewardWinner(test: Field, solution: Field, computation: Field, result: Bool) {
