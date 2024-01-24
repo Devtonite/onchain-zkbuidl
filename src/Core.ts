ca9318943b318e5ef981a9e0756189eee9fb3992
devtonite <devtonite001@gmail.com>
2024-01-24 04:43:01 -0600

    Fix publishBounty proving error

--- src/Core.ts
+++ src/Core.ts
@@ -1,4 +1,4 @@
-import { Field, SmartContract, state, State, method, Bool, Poseidon, UInt32, UInt64, PrivateKey, AccountUpdate } from 'o1js';
+import { Field, SmartContract, state, State, method, Bool, Poseidon, UInt32, UInt64, PrivateKey, AccountUpdate, Provable } from 'o1js';
 import { BountyQuest } from './builder/BuildQuest';
 import { BountyKey } from './hunter/CommitKey';
 
@@ -14,47 +14,39 @@ export class Core extends SmartContract {
   }
 
   @method publishBountyQuest(quest: BountyQuest) {    
+    this.openBountyQuest.getAndRequireEquals();
+
     let allAccountUpdates = AccountUpdate.create(this.sender)
-
+    
+    let now = this.network.timestamp.getAndRequireEquals();    
+    quest.bountyDuration.assertGreaterThan(now);
+    
     let balance = allAccountUpdates.account.balance.getAndRequireEquals();
-    let questTokenReward = quest.tokenRewardAmount;
-    balance.assertGreaterThanOrEqual(questTokenReward)
-    allAccountUpdates.send({to: this.address, amount: questTokenReward})
+    balance.assertGreaterThanOrEqual(quest.tokenRewardAmount)
+    allAccountUpdates.send({to: this, amount: quest.tokenRewardAmount})
     allAccountUpdates.requireSignature();
-
-    let duration = quest.bountyDuration;
-    let now = this.network.timestamp.getAndRequireEquals();
-    let bountyOpenTill = now.add(duration);
-
-    let timedQuest = new BountyQuest({
-      builderID: quest.builderID,
-      testHash: quest.testHash,
-      tokenRewardAmount: quest.tokenRewardAmount,
-      bountyDuration: bountyOpenTill
-    })
-
-    this.openBountyQuest.set(timedQuest);
-
+    
+    this.openBountyQuest.set(quest);
   }
 
-  @method commitBountyKey(solution: BountyKey) {
-    let openBounty = this.openBountyQuest.getAndRequireEquals();
-    let now = this.network.timestamp.getAndRequireEquals();
-    now.assertLessThanOrEqual(openBounty.bountyDuration);
-    this.merkledBountyKeys.set(solution)
+  // @method commitBountyKey(solution: BountyKey) {
+  //   let openBounty = this.openBountyQuest.getAndRequireEquals();
+  //   let now = this.network.timestamp.getAndRequireEquals();
+  //   now.assertLessThanOrEqual(openBounty.bountyDuration);
+  //   this.merkledBountyKeys.set(solution)
 
-  }
+  // }
   
-  @method rewardWinner(test: Field, solution: Field, computation: Field, result: Bool) {
-    let onStateQuest = this.openBountyQuest.getAndRequireEquals();
-    onStateQuest.testHash.assertEquals(Poseidon.hash([test]));
-    let onStateKey = this.merkledBountyKeys.getAndRequireEquals();
-    onStateKey.solutionHash.assertEquals(Poseidon.hash([solution]));
-    // trust assumption: if caller of this function received the test and solution, their computation and result is honest and accurate.
-    result.assertEquals(Bool(true));
+  // @method rewardWinner(test: Field, solution: Field, computation: Field, result: Bool) {
+  //   let onStateQuest = this.openBountyQuest.getAndRequireEquals();
+  //   onStateQuest.testHash.assertEquals(Poseidon.hash([test]));
+  //   let onStateKey = this.merkledBountyKeys.getAndRequireEquals();
+  //   onStateKey.solutionHash.assertEquals(Poseidon.hash([solution]));
+  //   // trust assumption: if caller of this function received the test and solution, their computation and result is honest and accurate.
+  //   result.assertEquals(Bool(true));
 
-    // this.verifiableComputation.set(solution);
-    this.send({to: onStateKey.hunterID, amount: onStateQuest.tokenRewardAmount})
-  }
+  //   // this.verifiableComputation.set(solution);
+  //   this.send({to: onStateKey.hunterID, amount: onStateQuest.tokenRewardAmount})
+  // }
 
 }
